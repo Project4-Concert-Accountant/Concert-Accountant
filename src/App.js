663d0c3e09eaf53cb4205de09107987afc1d394a
kunal445 <73367589+kunal445@users.noreply.github.com>
2022-05-06 14:22:15 -0400

    created the base api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+<<<<<<< Updated upstream
 import ApiCall from "./components/ApiCall"
 
+=======
+import { useEffect } from 'react';
+>>>>>>> Stashed changes
 
 function App() {
+
+  const url = new URL('https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=dAuzWaR4q5pH90ZcC6CpJPHDjH7Gjzz8&city=Toronto&segmentName=sports')
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+  }, [])
   return (
     <div className="App">
       <h1>Api Call</h1>
