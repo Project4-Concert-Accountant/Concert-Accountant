72f98ca948d3ee195dccf6faf9c66011570162fc
JairoCalderon <j.calderoneng@gmail.com>
2022-05-06 14:59:16 -0400

    adding axios api call

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import ApiCall from './components/ApiCall';
 
 function App() {
 
-  const url = new URL('https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=dAuzWaR4q5pH90ZcC6CpJPHDjH7Gjzz8&city=Toronto&segmentName=sports')
-
-  useEffect(() => {
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    })
-  }, [])
-
- 
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Here is my Api call</h1>
+      <ApiCall />
     </div>
   );
 }
--- src/components/ApiCall.js
+++ src/components/ApiCall.js
@@ -1,10 +1,26 @@
 import axios from "axios";
 import { useEffect } from "react";
 
-const ApiCall=()=>{
+const ApiCall = () => {
 
-    
-    return(
+    useEffect(() => {
+        axios({
+            url: "https://app.ticketmaster.com/discovery/v2/events.json?",
+            params: {
+                apikey: "dAuzWaR4q5pH90ZcC6CpJPHDjH7Gjzz8",
+                format: "json",
+                size: 10,
+                city: "Toronto",
+                segmentName: "music"
+            }
+        })
+            .then((res) => {
+                console.log(res)
+            })
+
+    }, [])
+
+    return (
         <div>
 
         </div>
