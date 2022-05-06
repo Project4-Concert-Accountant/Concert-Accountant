c54543b776d7ad3fa85b343e1cc593f35992988f
kunal445 <73367589+kunal445@users.noreply.github.com>
2022-05-06 14:23:30 -0400

    created the base api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
+import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-import ApiCall from "./components/ApiCall"
-
-=======
 import { useEffect } from 'react';
->>>>>>> Stashed changes
 
 function App() {
 
@@ -17,9 +13,25 @@ function App() {
       console.log(data);
     })
   }, [])
+
+ 
+
   return (
     <div className="App">
-      <h1>Api Call</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
