2569534f48ee4be539f76dd03c4aff0a9cb4d63d
tingjc <jean.claude.ting@gmail.com>
2022-05-08 14:39:53 -0400

    Added some Regex-like logic to budget input

--- src/components/ListBudgetForm.js
+++ src/components/ListBudgetForm.js
@@ -1,20 +1,34 @@
+import { useState } from "react";
+
 const ListBudgetForm = () => {
+    //create state for budget value to limit input to numbers only
+    const [val, setVal] = useState("");
+    const numberCheck = (event) => {
+        //on input change, ; check if input matches pattern attribute (.validity.patternMismatch); if it is valid (false), set val state to input; if it is invalid (true) set target.value to val
+        event.target.validity.patternMismatch === false ?  setVal(event.target.value) : event.target.value = val;
+        
+    }
     return (
         <div className="listNameBudget">
             <form>
                 <p>Create a budget!</p>
-                <label for="listName" className="sr-only">List Name</label>
+                <label htmlFor="listName" className="sr-only">List Name</label>
                 <input 
                 type="text" 
                 id="listName" 
                 placeholder="Name the list" />
 
-                <label for="budget" className="sr-only">Budget</label>
+                <label htmlFor="budget" className="sr-only">Budget</label>
                 <input 
                 type="text" 
                 pattern="[0-9]*"
+                title="numbers only"
                 id="budget" 
-                placeholder="How much is your budget?"/>
+                placeholder="How much is your budget?"
+                //event listener
+                onChange={
+                    numberCheck
+                }/>
                 <button type="submit">Create a budget!</button>
             </form>
         </div>
