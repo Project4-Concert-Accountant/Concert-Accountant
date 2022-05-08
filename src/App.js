c8a4cfd79bd4cd4f307f497dae6dac92deb14db6
tingjc <jean.claude.ting@gmail.com>
2022-05-08 13:00:19 -0400

    filled out basic form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import ApiCall from './components/ApiCall';
-
+import ListBudgetForm from './components/ListBudgetForm';
 function App() {
 
   return (
     <div className="App">
       <h1>Here is my Api call</h1>
+      <ListBudgetForm />
       <ApiCall />
     </div>
   );
--- src/components/ListBudgetForm.js
+++ src/components/ListBudgetForm.js
@@ -1,6 +1,23 @@
 const ListBudgetForm = () => {
     return (
-        
+        <div className="listNameBudget">
+            <form>
+                <p>Create a budget!</p>
+                <label for="listName" className="sr-only">List Name</label>
+                <input 
+                type="text" 
+                id="listName" 
+                placeholder="Name the list" />
+
+                <label for="budget" className="sr-only">Budget</label>
+                <input 
+                type="text" 
+                pattern="[0-9]*"
+                id="budget" 
+                placeholder="How much is your budget?"/>
+                <button type="submit">Create a budget!</button>
+            </form>
+        </div>
     )
 }
 
