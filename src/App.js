f66962efd0849746a1d5ab941ef38ea1fe499057
clairehousez <97857341+clairehousez@users.noreply.github.com>
2022-05-10 11:28:39 -0400

    created route to search page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import ApiCall from './components/ApiCall';
 import ListBudgetForm from './components/ListBudgetForm';
+import { Route, Routes } from 'react-router-dom';
+import SearchPage from './components/SearchPage';
 function App() {
 
   return (
@@ -8,6 +10,9 @@ function App() {
       <h1>Here is my Api call</h1>
       <ListBudgetForm />
       <ApiCall />
+      <Routes>
+        <Route path='/' element={<SearchPage />}/>
+      </Routes>
     </div>
   );
 }
