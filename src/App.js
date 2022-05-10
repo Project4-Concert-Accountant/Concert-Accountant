26cb77ee037fae5e243e9ada34f855032c1a4658
clairehousez <97857341+clairehousez@users.noreply.github.com>
2022-05-10 12:44:49 -0400

    updates to search page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import ApiCall from './components/ApiCall';
 import ListBudgetForm from './components/ListBudgetForm';
 import { Route, Routes } from 'react-router-dom';
 import SearchPage from './components/SearchPage';
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <h1>Here is my Api call</h1>
       <ListBudgetForm />
-      <ApiCall />
       <Routes>
         <Route path='/' element={<SearchPage />}/>
       </Routes>
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,7 +1,67 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import EventInfo from "./EventInfo";
+
 const SearchPage = () => {
+    const [userSearch, setUserSearch] = useState("")
+
+    const [data, setData] = useState([]);
+    const [paidArray, setPaidArray] = useState([]);
+    // use the free array for broke mode later
+    const [freeArray, setFreeArray] = useState([]);
+
+    const apiCall = (event) => {
+        event.preventDefault()
+
+        axios({
+            url: "https://app.ticketmaster.com/discovery/v2/events.json?",
+            params: {
+                apikey: "dAuzWaR4q5pH90ZcC6CpJPHDjH7Gjzz8",
+                format: "json",
+                size: 20,
+                sort: 'date,asc',
+                city: "Toronto",
+                segmentName: userSearch,
+            }
+        })
+            .then((res) => {
+                // the useful data from api
+                const apiData = res.data._embedded.events
+                setData(apiData);
+            })
+
+                // creating a copy so we dont mutate the original data
+                const copyOfData = [...data];
+                const primaryPaidArray = [];
+                const primaryFreeArray = [];
+
+                copyOfData.forEach(eachItem => {
+                    if(eachItem.priceRanges && eachItem.priceRanges[0].min > 0){
+                        primaryPaidArray.push(eachItem);
+                    }
+                    else {
+                        primaryFreeArray.push(eachItem);
+                    }
+                })
+
+                setPaidArray(primaryPaidArray);
+                setFreeArray(primaryFreeArray);
+                console.log(data)
+    }
+
+    const handleUserSearch = (event) => {
+        const searchQuery = event.target.value;
+        setUserSearch(searchQuery)
+        console.log(userSearch)
+    }
+
     return (
-        <div>SearchPage
-            
+        <div>
+            <form onSubmit={apiCall}>
+                <input onChange={handleUserSearch} type="text" id="search" name="search" />
+                <button>BUTTONNNNN</button>
+            </form>
+            {paidArray.length === 0 ? null : <EventInfo eventArray = { paidArray } />}
         </div>
     )
 }
